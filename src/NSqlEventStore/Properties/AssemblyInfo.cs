a1f86e779c73ed4436c9cb8f3b8aa17f658eda0c
NPehrsson <niclas.pehrsson@cignium.com>
2016-12-31 02:04:06 +0100

    Trying to change nuget name

--- src/NSqlEventStore/Properties/AssemblyInfo.cs
+++ src/NSqlEventStore/Properties/AssemblyInfo.cs
@@ -1,7 +1,13 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("NSqlEventStore")]
+//[assembly: AssemblyTitle("NSqlEventStore")]
+//[assembly: AssemblyDescription("")]
+//[assembly: AssemblyProduct("NSqlEventStore")]
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssemblyTitle("NSqlEventSourcing")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyProduct("NSqlEventStore")]
+[assembly: AssemblyProduct("NSqlEventSourcing")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
